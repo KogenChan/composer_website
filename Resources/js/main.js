82927d1d013fd2386c2ef2721036899bcab7af40
Luca Simone <lucasimonecomposer@gmail.com>
2025-07-15 17:09:18 +0200

    updated navbar/added header mobile view

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -3,8 +3,6 @@ $icon_1 = document.querySelector('.bi-list');
 $icon_2 = document.querySelector('.bi-x');
 $toggler.onclick = changeIcon;
 
-console.log($toggler, $icon_1, $icon_2)
-
 function changeIcon() {
     $icon_1.classList.toggle('opacity');
     $icon_2.classList.toggle('opacity');
