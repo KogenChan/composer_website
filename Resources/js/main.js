698f8acdcb95278e548ae7da9a542c139d30cd2e
Luca Simone <lucasimonecomposer@gmail.com>
2025-07-17 20:17:19 +0200

    fixed the credits section to read from json file instead

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,38 +1,42 @@
-$toggler = document.querySelector('.navbar-toggler');
-$icon_1 = document.querySelector('.bi-list');
-$icon_2 = document.querySelector('.bi-x');
-$toggler.onclick = changeIcon;
+const toggler = document.querySelector('.navbar-toggler');
+const icon_1 = document.querySelector('.bi-list');
+const icon_2 = document.querySelector('.bi-x');
+toggler.onclick = changeIcon;
 
 function changeIcon() {
     $icon_1.classList.toggle('opacity');
     $icon_2.classList.toggle('opacity');
 }
 
-$credits_section = document.getElementById('credits');
-$credits = [
-    { 
-        "name": "The Ferryman",
-        "img": "/Resources/media/ferryman.jpg",
-        "role": "Composer"
-    },
-    { 
-        "name": "Threaded Memories",
-        "img": "/Resources/media/memories.png",
-        "role": "Composer/Sound Designer"
-    },
-    { 
-        "name": "The Sleeper Beneath",
-        "img": "/Resources/media/sleeper.png",
-        "role": "Composer/Sound Designer"
-    },
-    { 
-        "name": "Mediapep",
-        "img": "/Resources/media/mediapep.png",
-        "role": "Composer"
+
+const credits_section = document.getElementById('credits');
+let delay = 0;
+
+async function getCredits() {
+    try {
+        const response = await fetch('../../index.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const credits = await response.json();
+
+        credits.forEach(credit => {
+            credits_section.innerHTML += `
+                <div class="col-6 col-lg-4 col-xl-3 pb-5" 
+                    data-aos="fade-left" 
+                    data-aos-duration="1500" 
+                    data-aos-delay="${delay}">
+                    <img class="credit-img" src="${credit.img}" alt="${credit.name}">
+                    <p class="text-white pt-1">${credit.role}</p>
+                </div>
+            `;
+            delay = Math.round(((delay + 250) * 0.9) / 50) * 50;
+        });
+
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
     }
-];
+}
 
-
-$credits.forEach(credit => {
-    $credits_section.innerHTML += '<div class="col-6 col-lg-4 col-xl-3 pb-5"><img class="credit-img"' + 'src="' + credit.img + '" alt="' + credit.name + '"><p class="text-white pt-1">' + credit.role + '</div>'
-});
+getCredits();
