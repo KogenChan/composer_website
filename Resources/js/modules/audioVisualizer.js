27c495c30214931068a9f8e48fe8873f85e107a2
Luca Simone <lucasimonecomposer@gmail.com>
2025-08-08 19:49:10 +0200

    apparently most paths work locally even if they're wrong uh

--- Resources/js/modules/audioVisualizer.js
+++ Resources/js/modules/audioVisualizer.js
@@ -1,4 +1,4 @@
-import WaveSurfer from '../../../node_modules/wavesurfer.js/dist/wavesurfer.js';
+import WaveSurfer from '../../../../node_modules/wavesurfer.js/dist/wavesurfer.js';
 
 const wavesurfer = WaveSurfer.create({
     container: '#waveform',
