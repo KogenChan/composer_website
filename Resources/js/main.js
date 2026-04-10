1772f89a505b247c893cc43562e3a1624434229d
Evelyn <narunaga@live.it>
2026-04-10 13:52:10 +0200

    updated fetch url

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -5,7 +5,7 @@ let delay = 0;
 
 async function getCredits() {
    try {
-      const response = await fetch('https://lucasimone.net/credits.min.json');
+      const response = await fetch('https://evelynsimone.com/credits.min.json');
       if (!response.ok) {
          throw new Error(`HTTP error! Status: ${response.status}`);
       }
@@ -41,7 +41,7 @@ const playlist_section = document.getElementById('playlist');
 
 async function getPlaylist() {
    try {
-      const response = await fetch('https://lucasimone.net/playlist-min.json');
+      const response = await fetch('https://evelynsimone.com/playlist-min.json');
       if (!response.ok) {
          throw new Error(`HTTP error! Status: ${response.status}`);
       }
